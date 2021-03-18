ffbfd1cda418072466ebbe2edd5c18f06dd5a3b9
Sanhos <sanhos.95@gmail.com>
2021-03-18 11:11:52 +0200

    New v2

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-let money = prompt("Ваш бюджет на месяц?",""), 
-    time = prompt("Введите дату в формате YYYY-MM-DD","");
+'use strict';
 
-let appData =  {
-    buget: money, expenses:{
-
-    }
-}
-
-let num = 50;              //перемінна num = 50
-while (num < 55){          //перевіряємо чи num менше 55
-    console.log(num);      //виводимо в консольку значення на даний момент
-    num++;                 //додаємо по 1 поки умова не виконається
-}
-
-let num = 50;
-do {
-    console.log(num);
-    num++;
-}
-while (num < 55)
+alert( null || 2 && 3 || 4 );
